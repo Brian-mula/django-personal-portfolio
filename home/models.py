288db4742e23b2812acbc9b16b7fdb3fb64f1896
Brian-mula <mulatiwangilabrian@gmail.com>
2022-12-10 14:59:05 +0300

    models

--- home/models.py
+++ home/models.py
@@ -3,4 +3,6 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
-    pass
+    title=models.CharField(max_length=50)
+    name=models.CharField(max_length=50),
+    body=models.TextField()
