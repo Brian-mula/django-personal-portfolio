3d6589977815e31d8b82442f407772f84fc8f403
Brian-mula <mulatiwangilabrian@gmail.com>
2022-12-13 10:24:54 +0300

    implimented authentication

--- accounts/views.py
+++ accounts/views.py
@@ -5,4 +5,15 @@ from django.shortcuts import redirect, render
 
 # Create your views here.
 def login_user(request):
-    return render(request,'authentication/login.html')
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('blogs')
+        else:
+            messages.success(request,("There was an error logging in"))
+            return redirect('login')
+    else:
+        return render(request,'authentication/login.html')
