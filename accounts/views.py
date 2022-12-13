d6c5f0215df5027e63dd4b3ae5fee2b6fd66da3d
Brian-mula <mulatiwangilabrian@gmail.com>
2022-12-13 09:00:58 +0300

    added login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+
 
 # Create your views here.
+def login_user(request):
+    return render(request,'authentication/login.html')
