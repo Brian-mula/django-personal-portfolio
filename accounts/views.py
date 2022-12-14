b748bde981cdc2cd169f5315f37f6c0e3a7b5321
Brian-mula <mulatiwangilabrian@gmail.com>
2022-12-14 16:32:31 +0300

    added user registration function

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
 
 
@@ -24,4 +25,18 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request,'authentication/register.html')
+    form=UserCreationForm(request.POST or None)
+    if request.method=="POST":
+        
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            pasword=form.cleaned_data['password']
+            user=authenticate(request,username=username,pasword=pasword)
+            login(request,user)
+            return redirect('blogs')
+        else:
+            form=UserCreationForm()
+            return 
+    else:
+        return render(request,'authentication/register.html',{'form':form})
